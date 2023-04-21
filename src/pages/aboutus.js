6a3fcc20900106927442c3a5ddb985ba868352ef
FHP187 <gjimenez@jaumebalmes.net>
2023-04-21 16:55:45 +0200

    Commit oficial

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import Helmet from 'react-helmet';
-
+// Commit main
 const AboutUsPage = () => {
     return (
         <Container>
